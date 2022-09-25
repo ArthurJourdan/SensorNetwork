94c72b02d9aa8a84f30ff503e31fa808e65c0d27
jeremy <jong0053@student.monash.edu>
2022-09-25 23:14:05 +1000

    balloon thread functions into seperate file proper implementation of sentinel value reading thread polling for available messages

--- include/base_station.h
+++ include/base_station.h
@@ -1,9 +1,10 @@
-#ifndef BASE_STATION
-#define BASE_STATION
+#ifndef BASE_STATION_H
+#define BASE_STATION_H
 
 #include "mpi_utils.h"
 
 void base_station(mpi_info_t process);
 
+extern int THREADS_EXIT;
 
 #endif 
